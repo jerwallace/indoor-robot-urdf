e04b82c41a85c8cfe50a96ce497689b6b96bc2ef
Ubuntu <ubuntu@ip-172-31-80-227.ec2.internal>
2021-04-08 14:06:33 +0000

    latest

--- workspace/src/bridge_example/src/lidar_print.cc
+++ workspace/src/bridge_example/src/lidar_print.cc
@@ -13,25 +13,19 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
     }
 }
 
-// Standard C++ entry point
 int main(int argc,char**argv) {
     
-    // Announce this program to the ROS master as a "node" called "hello_world_node"
     ros::init(argc,argv,"laser_scan_print");
     
-    // Start the node resource managers (communication, time, etc)
     ros::start();
     
     ros::NodeHandle n;
     
     ros::Subscriber sub = n.subscribe("laser_scan", 1000, laserCallback);
     
-    // Process ROS callbacks until receiving a SIGINT (ctrl-c)
     ros::spin();
     
-    // Stop the node's resources
     ros::shutdown();
-    // Exit tranquilly
 
     return 0;
 }
